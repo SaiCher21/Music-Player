7abf4002d0576c96c2f4b0ca6e7a4219c075d8f0
sai preetham <saipreethcheru@gmail.com>
2023-10-10 20:10:46 +0530

    make

--- assets/js/music-list.js
+++ assets/js/music-list.js
@@ -1,43 +1,43 @@
 let allMusic=[
     {
-        name:"Harley Bird - Home",
-        artist:"Jordan Schor",
+        name:"Living Hell",
+        artist:"Bella poarch",
         song:"music-1",
         src:"music-1"
     },
     {
-        name:"As You Fade Away",
-        artist:"Michael Jackson",
+        name:"Leviating",
+        artist:"Dua lipa",
         song:"music-2",
         src:"music-2"
     },
     {
-        name:"Mike Archangelo",
-        artist:"Sabrina",
+        name:"Dynamite",
+        artist:"BTS",
         song:"music-3",
         src:"music-3"
     },
     {
-        name:"Ruthless",
-        artist:"NEFFEX",
+        name:"Dolls",
+        artist:"Bella poarch",
         song:"music-4",
         src:"music-4"
     },
     {
-        name:"Jim - Sun Goes Down",
-        artist:"Jim Yosef x Roy",
+        name:"We don't any more",
+        artist:"Charlie puth, Selena Gomez",
         song:"music-5",
         src:"music-5"
     },
     {
-        name:"Lost Sky - Vision NCS",
-        artist:"NCS release",
+        name:"Shape of you",
+        artist:"Ed sheeran",
         song:"music-6",
         src:"music-6"
     },
     {
-        name:"It's Only Worth",
-        artist:"Animation Coding",
+        name:"See you again ",
+        artist:"charlie puth, Wiz Khalifa",
         song:"music-7",
         src:"music-7"
     }
