e5529b2d97d878d43d4a98620e49dc0d3a361042
sai preetham <saipreethcheru@gmail.com>
2023-10-10 10:07:35 +0530

    js

---
+++ script.js
@@ -0,0 +1,12 @@
+('use strict');
+
+const wrapper = document.querySelector(".wrapper"),
+img = document.querySelector(".img"),
+name = document.querySelector(".name"),
+artist = document.querySelector(".artist"),
+playPauseBtn = document.querySelector(".play-pause"),
+preBtn = wrapper.querySelector("#prev"),
+nextBtn = wrapper.querySelector("#next"),
+audio = wrapper.querySelector("#main-audio"),
+progressArea = document.querySelector(".progress-area"),
+progressBar  = document.querySelector("progress-bar");
