6e33d6de644cb02ed078a3e9ef6a95f3eb916d38
sai preetham <saipreethcheru@gmail.com>
2023-10-10 19:46:36 +0530

    changes

---
+++ assets/js/music-list.js
@@ -0,0 +1,44 @@
+let allMusic=[
+    {
+        name:"Harley Bird - Home",
+        artist:"Jordan Schor",
+        song:"music-1",
+        src:"music-1"
+    },
+    {
+        name:"As You Fade Away",
+        artist:"Michael Jackson",
+        song:"music-2",
+        src:"music-2"
+    },
+    {
+        name:"Mike Archangelo",
+        artist:"Sabrina",
+        song:"music-3",
+        src:"music-3"
+    },
+    {
+        name:"Ruthless",
+        artist:"NEFFEX",
+        song:"music-4",
+        src:"music-4"
+    },
+    {
+        name:"Jim - Sun Goes Down",
+        artist:"Jim Yosef x Roy",
+        song:"music-5",
+        src:"music-5"
+    },
+    {
+        name:"Lost Sky - Vision NCS",
+        artist:"NCS release",
+        song:"music-6",
+        src:"music-6"
+    },
+    {
+        name:"It's Only Worth",
+        artist:"Animation Coding",
+        song:"music-7",
+        src:"music-7"
+    }
+];
--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 ('use strict');
 
 const wrapper = document.querySelector(".wrapper"),
-img = document.querySelector(".img"),
-name = document.querySelector(".name"),
-artist = document.querySelector(".artist"),
-playPauseBtn = document.querySelector(".play-pause"),
-preBtn = wrapper.querySelector("#prev"),
-nextBtn = wrapper.querySelector("#next"),
-audio = wrapper.querySelector("#main-audio"),
-progressArea = document.querySelector(".progress-area"),
-progressBar  = document.querySelector("progress-bar");
+      musicImg = document.querySelector(".img"),
+      musicName = document.querySelector(".name"),
+      musicArtist = document.querySelector(".artist"),
+      playPauseBtn = document.querySelector(".play-pause"),
+      preBtn = wrapper.querySelector("#prev"),
+      nextBtn = wrapper.querySelector("#next"),
+      mainAudio = wrapper.querySelector("#main-audio"),
+      progressArea = document.querySelector(".progress-area"),
+      progressBar  = document.querySelector("progress-bar");
+
+const musicIndex = Math.floor((Math.random() * allMusic.length) + 1);
+isMusicPaused = true;
+window.addEventListener("load", () =>{
+    loadMusic(musicIndex);
+});
+
+function loadMusic(indexNum){
+    musicName.innertext = allMusic[indexNum - 1].name;
+    musicArtist.innerText = allMusic[indexNum - 1].artist;
+    musicImg.src = `assets/images/${all[indexNum - 1].src}.jpg`;
+    musicAudio.src = `assets/images/${all[indexNum - 1].src}.jpg`;
+}
